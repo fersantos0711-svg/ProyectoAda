4b97e47e3752b38fdc0819e2e357b9487d27c39d
fersantos0711 <fersantos0711@gmail.com>
2025-12-13 23:59:34 -0600

    update clase8

---
+++ OneDrive/Desktop/AdaTrabajos/clase6/saludo.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+int main() { 
+printf("Hola Mundo"); 
+return 0; 
+} 
+#include <stdio.h> 
+int main() { 
+printf("Hola Mundo"); 
+return 0; 
+} 
+#include <stdio.h> 
+int main() { 
+printf("Hola Mundo"); 
+return 0; 
+} 
+#include <stdio.h> 
+int main() { 
+printf("Hola Mundo"); 
+return 0; 
+} 
+console.log("Este es un archivo de código C llamado saludo.c que imprime 'Hola Mundo' en la consola.");
+#include <stdio.h> 
+int main() { 
+printf("Hola Mundo"); 
+return 0; 
+} 
+console.log("Hola Mundo"); 
+
